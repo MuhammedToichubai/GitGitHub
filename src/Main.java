7d90a297dfad073804f48ec4be0e128bc451b4c0
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2024-01-09 16:11:54 +0600

    add method

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,37 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Hello java 12");
+
+
+
+
+
+
+
+
+
+
+
+
+        method();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void method(){
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello java 12");
+        System.out.println("Hello java 12");
+        System.out.println("Hello java 12");
     }
 }
